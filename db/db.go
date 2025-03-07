8e99f7a52df08cb79352bb47e5bdafd6a06d52a1
Katarzyna Rak <katarzynarak9@gmail.com>
2025-03-07 16:26:30 +0100

    Add database initialization and connection setup
    Created a database package to manage PostgreSQL connection using GORM.
    This setup includes environment variables for database credentials.

---
+++ db/db.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file: ", err)
+	}
+
+	dsn := "host=" + os.Getenv("DB_HOST") +
+		" user=" + os.Getenv("DB_USER") +
+		" password=" + os.Getenv("DB_PASSWORD") +
+		" dbname=" + os.Getenv("DB_NAME") +
+		" port=" + os.Getenv("DB_PORT") +
+		" sslmode=" + os.Getenv("SSL_MODE")
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to the database: ", err)
+	}
+
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"dictionary-app/db"
+)
+
+func main() {
+	db.InitDB()
+}
