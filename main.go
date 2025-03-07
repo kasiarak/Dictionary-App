91c715c98dc309c5d9ab7b00ae8df07e1954195d
Katarzyna Rak <katarzynarak9@gmail.com>
2025-03-07 21:54:13 +0100

    Set up main server for GraphQL API
    Initializes the database, creates the GraphQL schema, and starts the HTTP server. The GraphQL API is now accessible at /dictionary.

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"os"
+
 	"dictionary-app/db"
+	"dictionary-app/graphql"
+
+	"github.com/graphql-go/handler"
 )
 
 func main() {
 	db.InitDB()
+
+	schema, err := graphql.CreateSchema()
+	if err != nil {
+		log.Fatalf("Error creating schema: %v", err)
+	}
+
+	h := handler.New(&handler.Config{
+		Schema: schema,
+		Pretty: true,
+	})
+
+	http.Handle("/dictionary", h)
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Println("GraphQL server running on http://localhost:" + port + "/dictionary")
+	http.ListenAndServe(":"+port, nil)
 }
